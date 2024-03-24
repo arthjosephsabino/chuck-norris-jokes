65dec006c2b8b7dbd9604d6fbc979cd6e5f6d4e2
arthjosephsabino <arthjoseph.sabino@gmail.com>
2024-03-24 19:13:55 +0800

    get random joke by category API added

--- backend/src/controllers/jokes.ts
+++ backend/src/controllers/jokes.ts
@@ -3,20 +3,23 @@ import https from "node:https";
 import env from "../util/validateEnv";
 
 export const getRandomJoke: RequestHandler = async (req, res, next) => {
-  if (req) {
-    const request = https.get(`${env.API_URL}/jokes/random`, (response) => {
-      let data = "";
-      response.on("data", (chunk) => {
-        data = data + chunk.toString();
-      });
-      response.on("end", () => {
-        const body = JSON.parse(data);
-        res.status(200).json(body);
-      });
-    });
-    request.on("error", (error) => {
-      next(error);
-    });
-    request.end();
+  const { category } = req.body;
+  let url = `${env.API_URL}/jokes/random`;
+  if (category) {
+    url = `${url}?category=${category}`;
   }
+  const request = https.get(url, (response) => {
+    let data = "";
+    response.on("data", (chunk) => {
+      data = data + chunk.toString();
+    });
+    response.on("end", () => {
+      const body = JSON.parse(data);
+      res.status(200).json(body);
+    });
+  });
+  request.on("error", (error) => {
+    next(error);
+  });
+  request.end();
 };
