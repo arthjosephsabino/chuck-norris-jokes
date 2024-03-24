4ad84866c2b05fce40048234cd0719b217f760e6
arthjosephsabino <arthjoseph.sabino@gmail.com>
2024-03-24 17:47:16 +0800

    added error handling middlewares

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,11 @@
 import "dotenv/config"; 
 import express from "express";
+import { json } from "body-parser";
+import { errorHandler } from "./middlewares/errors";
 
 const app = express();
+app.use(json());
+//routes here
+app.use(errorHandler);
 
 export default app
